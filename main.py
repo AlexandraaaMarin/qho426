63b8da87bbcc695438b96b0788f87b6468ea60d6
AlexandraM07 <93544630+AlexandraaaMarin@users.noreply.github.com>
2021-11-09 21:04:14 +0000

    "Added code to demonstrate the use of different data types.".

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-#print("\u2665")
-#print("\u25c6")
-
-my_list = []
-n = int(input("Enter the number of lives:"))
-c=int(input("Enter the energy level:"))
-d=int(input("Enter the shield level:"))
-print("Health has been set.")
-print("Number of lives:")
-for i in range(0, n):
-    print('\u2665', end=" ")
-print("\n")
 
 
-print("Energy level:")
-for i in range(0,c):
-    print('\u25c6', end=" ")
-print("\n")
 
-
-print ("Shield level:")
-for i in range(0,d):
-    print('\u25c6',end=" ")
- 
-print("\n")
+name=input("What is your name human? ")
+age=input("What is your age?(in years) ")
+height=float(input("How tall are you?(in meters)"))
+weight=float(input("How much do you weight(in kilograms)"))
+bmi= weight / (height) **2
+print(name,"you are "+str(age),"and","your BMI is "+str(bmi))
 
