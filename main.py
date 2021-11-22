356123a041af86f1e9128af30784799268a2941e
AlexandraM07 <93544630+AlexandraaaMarin@users.noreply.github.com>
2021-11-22 19:00:45 +0000

    Added code to review the use of a decisions, nesting and logical operators.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-print("How many books should I read?")
-books_to_read = int(input())
-
-books_removed = 0
-
-print()
-
-while (books_removed < books_to_read):
-    print("Book to be read...")
-    books_removed = books_removed + 1
+print("Select your ride:")
+print("1. Bike")
+print("2. Car")
+print("3. SUV")
+choice=int(input())
+if( choice == 1 ):
+  print( "You have selected Bike" )
+elif( choice == 2 ):
+  print( "You have selected Car" )
+elif( choice == 3 ):
+  print( "You have selected SUV" )
+else:
+  print("Wrong choice!")
